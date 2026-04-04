3edb3e954250c88fdae3a1f8d8a3be3cc8c97863
MarioSantaCruz <mariojuniorsct22@gmail.com>
2026-04-03 22:51:23 -0500

    v3

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 // @ts-ignore
+import avatarImg from '../assets/images/Foto.jpeg';
+// @ts-ignore
 import '../assets/styles/Main.scss';
 
 function Main() {
-
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={avatarImg} alt="Mario Santa Cruz" />
         </div>
         <div className="content">
           <div className="social_icons">
@@ -19,6 +20,11 @@ function Main() {
           </div>
           <h1>Mario Santa Cruz</h1>
           <p>Estudiante de Ingeniería de Sistemas y Computación</p>
+          <div className="mobile_social_icons">
+            <a href="https://github.com/MarioSantaCruz" target="_blank" rel="noreferrer">
+              <GitHubIcon/>
+            </a>
+          </div>
         </div>
       </div>
     </div>
